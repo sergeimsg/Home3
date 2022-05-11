955c37693dedb41b7aee00b76078c0571101155a
sergeimsg <sergei-msg@tuby>
2022-05-11 22:58:44 +0300

    Initial commit

---
+++ src/Task10.java
@@ -0,0 +1,20 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int x1 = 0;
+        int x2 = 1;
+        int x3 = 2;
+        int x4 = 3;
+        int x5 = 4;
+        int x6 = 5;
+        int x7 = 6;
+        int x8 = 7;
+        int x9 = 8;
+        int x10 = 9;
+        int z = x1+x2+x3+x4+x5+x6+x7+x8+x9+x10;
+        int y = z/10;
+        System.out.println(y);
+        double d = z / 10;
+        System.out.println(d + " - equal results");
+
+        }
+    }
